7495fdc0a827904f1404703f0461bb166652f3c6
Qinghui Xie <qxie@uw.edu>
2025-05-07 12:16:44 -0700

    update benchmark with datapoint values

--- benchmark.py
+++ benchmark.py
@@ -1,9 +1,11 @@
 import time
 import psutil
+import os
+from generate import generate_strings
 from basic_3 import seq_align_basic, process_memory  
 
-# Values of M + N 
-total_lengths = [16, 64, 128, 256, 384, 512, 768, 1024, 1280, 1536, 2048, 2560, 3072, 3584, 3968]
+#folder w the datapoints
+datapoints_folder = 'CSCI570_Project_Minimum_V1/datapoints'
 
 # Alignment parameters
 delta = 30
@@ -12,26 +14,23 @@ alpha = {"A": {"A": 0, "C": 110, "G": 48, "T": 94},
          "G": {"A": 48, "C": 118, "G": 0, "T": 110},
          "T": {"A": 94, "C": 48, "G": 110, "T": 0}}
 
-# This generates synthetic strings to test the total length
-import random
-def generate_random_dna_string(length):
-    return ''.join(random.choices(['A', 'C', 'G', 'T'], k=length))
+input_files = sorted([f for f in os.listdir(datapoints_folder) if f.startswith('in') and f.endswith('.txt')],
+                     key=lambda x: int(x[2:-4]))
 
+#table format
 
 print("M+N\tTime in MS (Basic)\tMemory in KB (Basic)")
 
-for total in total_lengths:
-    len1 = total // 2
-    len2 = total - len1
-    str1 = generate_random_dna_string(len1)
-    str2 = generate_random_dna_string(len2)
+for file in input_files:
+    path = os.path.join(datapoints_folder, file)
+    str1, str2 = generate_strings(path)
+    m_plus_n = len(str1) + len(str2)
 
-    # Time and memory
     start_time = time.time()
-    aligned1, aligned2, cost = seq_align_basic(str1, str2, delta, alpha)[:3]
+    _ = seq_align_basic(str1, str2, delta, alpha)  
     end_time = time.time()
-    time_taken_ms = (end_time - start_time) * 1000
 
+    time_taken_ms = (end_time - start_time) * 1000
     memory_kb = process_memory()
 
-    print(f"{total}\t{time_taken_ms:.2f}\t\t\t{memory_kb}")
+    print(f"{file}\t{m_plus_n}\t{time_taken_ms:.2f}\t\t{memory_kb}")
